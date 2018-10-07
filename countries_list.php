076a3225eda3867c560bda5f45494a5f5d0ada38
Sergey Zaligin <webdevjsphp@gmail.com>
2018-10-07 18:10:06 +0300

    fix

--- countries_list.php
+++ countries_list.php
@@ -10,7 +10,14 @@ include_once('lib/simple_html_dom.php');
 $c = Curl::app('https://www.yell.ru')->config_load('wiki.cnf');
 					
 // Get array data on data['headers'] and data['html']
-$data = $c->request('/spb/top/kadrovye-agentstva/?page=3');
+// Array countries
+$countries = [];
+$i = 0;
+while($i < 22){
+    $i++;
+    $data = $c->request('spb/top/kadrovye-agentstva/?page=' . $i);
+
+//$data = $c->request('/spb/top/kadrovye-agentstva/?page=3');
 
 // Build DOM
 $dom = str_get_html($data['html']);
@@ -21,22 +28,22 @@ $flags = $dom->find('.companies__item-title-text');
 // Count items
 $done = 0;
 
-// Array countries
-$countries = [];
+
 
 // Iteration $flags
 foreach ($flags as $span) {
     
-    $countries[$span->href] = $span->plaintext;  
+    $countries[$i][$span->href] = $span->plaintext;  
     
     $done++;       
 }
-    
+}  
 echo '<pre>';
 print_r($countries);
 echo '</pre>';
 
 // PHP in JSON
-file_put_contents('res/all', json_encode($countries));
 
+  
+  file_put_contents('res/all', json_encode($countries));
 echo "<br>done: $done<br>";
